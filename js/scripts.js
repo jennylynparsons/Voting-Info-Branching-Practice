524be40b76ae49ba6b8fdadcfbbdbaf74bcd9089
Jenny Parsons <jennylynparsons@gmail.com>
2023-06-12 20:54:58 -0600

    add voter age requirements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
 window.onload = function () {
+  const form = document.querySelector("form")
+
+  form.onsubmit = function (event) {
+    event.preventDefault();
+
+    let voterInfo = document.getElementById("voter");
+    voterInfo.setAttribute("class", "hidden");
+    let nonVoter = document.getElementById("non-voter")
+    nonVoter.setAttribute("class", "hidden");
+
+    const age = parseInt(document.querySelector("input#age").value);
+
+    
+    if (age >= 18) {
+      voterInfo.removeAttribute("class");
+    } else {
+      nonVoter.removeAttribute("class");
+    }
+  };
+};
